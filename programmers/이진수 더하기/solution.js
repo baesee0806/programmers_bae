3d0137710c80a97f1d3691f954677ff9258d0ffb
baesee <62400479+baesee0806@users.noreply.github.com>
2022-11-09 20:05:44 +0900

    updated at 2022. 11. 9. 오후 8:05:44

--- programmers/이진수 더하기/solution.js
+++ programmers/이진수 더하기/solution.js
@@ -1,6 +1,6 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120885
 
 function solution(bin1, bin2) {
-    var answer = parseInt(bin1,2) + parseInt(bin2,2);
-    return answer.toString(2);
+    var answer = '';
+    return answer;
 }
