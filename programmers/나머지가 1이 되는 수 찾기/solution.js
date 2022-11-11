cd00afbd82ba00ef7b29ebb8c854b6a3126a4431
baesee <62400479+baesee0806@users.noreply.github.com>
2022-11-11 16:58:42 +0900

    uploaded at 2022. 11. 11. 오후 4:58:41

---
+++ programmers/나머지가 1이 되는 수 찾기/solution.js
@@ -0,0 +1,9 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87389
+
+function solution(n) {
+    let answer = [];
+    for(let i=1;i<n;i++){
+        n%i == 1 ? answer.push(i):false
+    }
+    return Math.min(...answer)
+}
