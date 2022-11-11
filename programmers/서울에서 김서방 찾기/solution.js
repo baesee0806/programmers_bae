dee40d7eedf34980dfb276909459fae382c17619
baesee <62400479+baesee0806@users.noreply.github.com>
2022-11-11 17:39:28 +0900

    uploaded at 2022. 11. 11. 오후 5:39:28

---
+++ programmers/서울에서 김서방 찾기/solution.js
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+function solution(seoul) {
+    var answer = '';
+    let num = seoul.indexOf('Kim')
+    return `김서방은 ${num}에 있다`;
+}
