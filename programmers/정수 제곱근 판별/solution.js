0d61e5d205900855d7058ee0c0480d985480d88c
baesee <62400479+baesee0806@users.noreply.github.com>
2022-11-11 15:51:09 +0900

    updated at 2022. 11. 11. 오후 3:51:09

--- programmers/정수 제곱근 판별/solution.js
+++ programmers/정수 제곱근 판별/solution.js
@@ -1,7 +1,6 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12934
 
 function solution(n) {
-    var answer = 0;
-    return n%Math.sqrt(n)==0?Math.pow(Math.sqrt(n) +1,2):-1
+return n%Math.sqrt(n)==0?Math.pow(Math.sqrt(n) +1,2):-1
     
 }
