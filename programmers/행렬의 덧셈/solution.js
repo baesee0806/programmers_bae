b63d31da6e61b3857d60ac836a7351989e8792dd
baesee <62400479+baesee0806@users.noreply.github.com>
2022-11-13 21:34:47 +0900

    updated at 2022. 11. 13. 오후 9:34:47

--- programmers/행렬의 덧셈/solution.js
+++ programmers/행렬의 덧셈/solution.js
@@ -1,9 +1,14 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12950
 
 function solution(arr1, arr2) {
-  const answer = [];
-  arr1.forEach((row, rowIndex) => {
-    answer.push(row.map((col, colIndex) => col + arr2[rowIndex][colIndex]));
-  });
-  return answer;
+    var answer = [[]];
+    for (var i=0; i<arr1.length; i++){
+        answer[i] =[];
+        for(var j=0; j<arr1[i].length; j++){
+            answer[i].push(arr1[i][j] + arr2[i][j]);
+        }
+    }
+    return answer;
 }
+
+// 아 다풀었는데... 틀렸네... 하... 좀더 머리즘 쓰자....
