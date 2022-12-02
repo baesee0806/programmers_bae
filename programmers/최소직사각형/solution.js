c8fc1f2422434958f3f27247a3e97ef671df3f04
baesee <62400479+baesee0806@users.noreply.github.com>
2022-12-02 21:36:26 +0900

    uploaded at 2022. 12. 2. 오후 9:36:24

---
+++ programmers/최소직사각형/solution.js
@@ -0,0 +1,22 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+function solution(sizes) {
+    let answer = 0;
+    const flatArr = sizes.flat();
+    console.log(flatArr)
+    const xArr = []; //가로
+    const yArr = [];
+    flatArr.forEach((size, index) => {
+        if((index + 1) % 2 !== 0){
+            xArr.push(size)
+        } else yArr.push(size)
+    })
+    xArr.forEach((size, index) => {
+        if(size < yArr[index]){
+            xArr[index] = yArr[index];
+            yArr[index] = size;
+        } 
+    })
+    answer = (Math.max(...xArr)) * (Math.max(...yArr))
+    return answer;
+}
