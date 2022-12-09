559c363c6407b0d9d35f7f214354c508b9b158a4
baesee <62400479+baesee0806@users.noreply.github.com>
2022-12-09 19:52:09 +0900

    uploaded at 2022. 12. 9. 오후 7:52:08

---
+++ programmers/예산/solution.js
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12982
+
+function solution(d, budget) {
+    var answer = 0, sum = 0;
+    d.sort((a,b) => a - b);
+
+    for(let i = 0; i < d.length; i++){
+        answer++;
+        sum += d[i]
+
+        if(sum > budget)
+            answer--;
+    }
+  
+    return answer;
+}
