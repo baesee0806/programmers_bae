38b464f81777ca60232d207ea5c9218f0de746e6
baesee <62400479+baesee0806@users.noreply.github.com>
2022-12-20 11:38:16 +0900

    uploaded at 2022. 12. 20. 오전 11:38:14

---
+++ programmers/합성수 찾기/solution.js
@@ -0,0 +1,18 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120846
+
+function solution(n) {
+  const isPrime = num => {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return true;
+    }
+    return false;
+  };
+
+  let count = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (isPrime(i)) count += 1;
+  }
+
+  return count;
+}
