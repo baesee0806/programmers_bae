bcc0016bdbc6ac9ef70563094685bfa1cb359c13
baesee <62400479+baesee0806@users.noreply.github.com>
2023-03-23 22:09:49 +0900

    uploaded at 2023. 3. 23. 오후 10:09:48

---
+++ programmers/영어 끝말잇기/solution.js
@@ -0,0 +1,22 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12981
+
+function solution(n, words) {
+  let answer = [0, 0];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let p = i % n + 1;
+    let turn = Math.ceil((i + 1)/n);
+
+    if (i > 0) {
+      let last = words[i - 1].split("").pop();
+
+      if (i > words.indexOf(word) || words[i][0] !== last) {
+        answer = [p, turn];
+        break;
+      }
+    }
+  }
+
+  return answer;
+}
